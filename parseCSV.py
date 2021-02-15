0141d12571d81913063bc35473952bb82fc720b4
Moeez Arif <moeez.arif@alsyke.com>
2021-02-15 07:40:59 +0500

    Updated Class parsing csv and storing in list of objects

--- parseCSV.py
+++ parseCSV.py
@@ -1,23 +1,31 @@
-import pandas as pd
+import csv
+from movie_store import MovieStore
 
 
 class ParseCSV:
     def __init__(self, file_name):
-        self._df = pd.DataFrame()
+        self.file_reader = ''
         print('Opening {} for reading'.format(file_name))
         self.csv_file = open(file_name, mode='r')
         self.movies_information = []
 
     def create_file_reader(self):
-        self._df = pd.read_csv(self.csv_file)
+        self.file_reader = csv.reader(self.csv_file, delimiter=',')
 
-    def replace_null_values(self):
-        self._df = self._df.replace(r'\\N', 0, regex=True)
-        self._df['genres'] = self._df['genres'].replace(0, ' ')
-
-    def set_correct_data_types(self):
-        self.replace_null_values()
-        self._df["runtimeMinutes"] = pd.to_numeric(self._df['runtimeMinutes'])
+    def store_information(self):
+        index = 0
+        for csv_row in self.file_reader:
+            if index == 0:
+                index += 1
+            else:
+                self.movies_information.append(
+                    MovieStore(csv_row[0], csv_row[1], csv_row[2], csv_row[3], csv_row[4],
+                               csv_row[5], csv_row[6], csv_row[7]))
 
     def close_file(self):
         self.csv_file.close()
+
+    def display(self):
+        print(len(self.movies_information))
+        for _object in self.movies_information:
+            _object.print_information()
