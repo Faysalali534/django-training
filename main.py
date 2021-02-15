61e3318eb5b90dcfb4d5f8a41a145004e388e652
Moeez Arif <moeez.arif@alsyke.com>
2021-02-15 07:43:17 +0500

    Added method for argument parsing

--- main.py
+++ main.py
@@ -2,27 +2,29 @@ import os
 import timeit
 import json
 import argparse
-
 from calculations import *
 
-os.environ['csv_filename'] = '/Users/alsyketechnologies/PycharmProjects/pythonProject/Movies dataset - title.basics.csv'
 
-
-if __name__ == '__main__':
-    start = timeit.timeit()
+def argument_parser():
+    """
+        Argparse makes Validation easy if we enter argument of type other than we
+        we have already defined it will throw error
+    """
     parser = argparse.ArgumentParser(description='Fetch movies information')
     parser.add_argument('-r', '--year', type=int, help='Movie start Year')
     parser.add_argument('-g', '--genre', type=str, help='Movie Genre')
     parser.add_argument('-v', '--year_rating', type=int, help='High rated movie of year')
-    args = parser.parse_args()
-    csv_reading = Calculations(os.environ.get('csv_filename'), args)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    start = timeit.timeit()
+    args = argument_parser()
+    csv_reading = Calculations(os.environ['CSV_FILE'], args)
     csv_reading.create_file_reader()
-    csv_reading.set_correct_data_types()
-    csv_reading.calculate_first_argument_records()
-    csv_reading.calculate_second_argument_records()
-    csv_reading.calculate_third_argument_records()
+    csv_reading.store_information()
+    csv_reading.store_calculations_results()
     print(json.dumps(result_dictionary, indent=4, ensure_ascii=False))
     csv_reading.close_file()
     end = timeit.timeit()
     print(start - end)
-
