09e0932474cc262fa068ae40a6ce818221078fe2
Moeez Arif <moeez.arif@alsyke.com>
2021-02-19 12:46:58 +0500

    Updated Class for parsing and storing movies information

--- parseCSV.py
+++ parseCSV.py
@@ -1,4 +1,5 @@
 import csv
+
 from movie_store import MovieStore
 
 
@@ -13,19 +14,14 @@ class ParseCSV:
         self.file_reader = csv.reader(self.csv_file, delimiter=',')
 
     def store_information(self):
-        index = 0
+        next(self.file_reader)
         for csv_row in self.file_reader:
-            if index == 0:
-                index += 1
-            else:
-                self.movies_information.append(
-                    MovieStore(csv_row[0], csv_row[1], csv_row[2], csv_row[3], csv_row[4],
-                               csv_row[5], csv_row[6], csv_row[7]))
+            self.movies_information.append(
+                MovieStore(*csv_row))
 
     def close_file(self):
         self.csv_file.close()
 
     def display(self):
-        print(len(self.movies_information))
-        for _object in self.movies_information:
-            _object.print_information()
+        for movie_record in self.movies_information:
+            movie_record.print_information()
